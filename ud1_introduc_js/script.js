253502314aaa34571fb4c5d1e07e1b789c1ca752
RaquelFerFer <rferfer600@g.educaand.es>
2025-09-23 13:46:14 +0200

    ejercicios boletin 1

--- ud1_introduc_js/script.js
+++ ud1_introduc_js/script.js
@@ -8,25 +8,25 @@ function saludar() {
         }
     }
 
-// Ejercicio 2-1
+// Ejercicio 1-2
 function pintar_ruta(){
     console.log("El acceso a la ruta C:\\\\usuario\\tarda 1,23\", algo considerado \"lento\" en la actualidad.");
 }
 
-// Ejercicio 3-1
+// Ejercicio 1-3
 function pintar_ruta_V2(){
     var substring1 = "El acceso a la ruta C:\\\\usuario\\tarda 1,23\", algo";
     var substring2 = " considerado \"lento\" en la actualidad.";
     console.log(substring1 + substring2);
 }
 
-// Ejercicio 4-1
+// Ejercicio 1-4
 function muestra_potencia(){
     var num = 2*Math.pow(10,-9);
     console.log("El valor de 2*10^9 es: " + num);
 } 
 
-// Ejercicio 5-1
+// Ejercicio 1-5
 function distintas_bases(num){
     //Transforma num a binario
     var numDecimal = num.toString(10);
@@ -39,25 +39,39 @@ function distintas_bases(num){
     console.log("El número " + num + " en hexadecimal es: " + numHexa);
 }
 
-// Ejercicio 6-1
-function multiplicacion_cero(num1,num2){
-    var numMult= (num1/0)+num2;
-    console.log("El número " + num1 + " entre 0 y sumando 23 es: " + numMult);
+// Ejercicio 1-6
+function division_cero(num1,num2){
+    var numDiv= (num1/0)+num2;
+    console.log("El número " + num1 + " entre 0 y sumando 23 es: " + numDiv);
 }
 
-// Ejercicio 7-1
+// Ejercicio 1-7
 function forzar_NaN(){
     var NaN = 2 * "hola";
     console.log(NaN);
 }
 
-// Ejercicio 8-1
+// Ejercicio 1-8 
 function max_valor(number){
-    var maximo_valor = BigInt(number.MAX_VALUE)*2;
-    console.log("El máximo valor ")
+    var maximo_valor = BigInt(number.MAX_VALUE) * BigInt(2);
+    console.log("El máximo valor multiplicado por 2 es: " + maximo_valor.toString());
 }
 
-// Ejercicio 10-1 -  Let tiene visibilidad dentro de su anidación pero var tiene visibilidad 
+// Ejercicio 1-9 
+function suma_numero_cadena() {
+    let numero = 5;
+    let cadena = "10";
+    // a. Suma directa
+    let resultado1 = numero + cadena;
+    console.log("Suma directa (número + cadena):", resultado1); // Da "510" (concatenación)
+
+    // b. Suma asegurando suma numérica
+    let resultado2 = numero + Number(cadena);
+    console.log("Suma numérica (número + Number(cadena)):", resultado2); // Da 15 (suma real)
+    // Se ha convertido la cadena a número usando Number()
+}
+
+// Ejercicio 1-10 -  Let tiene visibilidad dentro de su anidación pero var tiene visibilidad
 // dentro de la misma función(es global)
 function check_ambito_variable(){
     let prueba = 5;
@@ -70,7 +84,7 @@ function check_ambito_variable(){
     console.log("No se puede ver el valor del segundo " + prueba2);
 }
 
-// Ejercicio 11
+// Ejercicio 1-11
 function definir_y_mostrar_array(){
     let array = ["Alberto","Juan","Sofia"];
     for(let i = 0; i < array.length; i++){
@@ -79,7 +93,7 @@ function definir_y_mostrar_array(){
     console.table(array);
 }
 
-// Ejercicio 12
+// Ejercicio 1-12
 function contar_y_temporizar(){
     let valor = 0;
     let initTime = new Date();
@@ -90,4 +104,62 @@ function contar_y_temporizar(){
     console.log("Valor: " + valor);
     console.log("El algoritmo ha tardado " + (endTime - initTime));
 
+}
+
+// Ejercicio 1-13
+
+// Ejercicio 1-14
+
+// Ejercicio 1-15
+function pedir_nombre(){
+    let seguir = true;
+
+    while(seguir){
+        let nombre = prompt("Introduzca su nombre:");
+        console.log("Hola " + nombre);
+        seguir = confirm("¿Desea continuar en el programa?"); //Confirm devuelve true o false
+        console.log("Has decidido continuar: " + seguir);
+    }
+
+    console.log("FIN del programa");
+    
+}
+
+// Ejercicio 1-16
+function pedir_datos(){
+    let nombre  = number(prompt("Introduzca su nombre:"));
+    let edad = prompt("Introduzca su edad:");
+    let ciudad = prompt("Introduzca su ciudad:");
+    let direccion = prompt("Introduzca su dirección:");
+    let telefono = number(prompt("Introduzca su teléfono:"));
+    if(edad**5  === telefono || ciudad.toUpperCase() === "Mairena del Alcor".toUpperCase()){
+        console.log("ENHORABUENA");
+    }else{
+        console.log("Lo siento, no cumples los requisitos");
+    }
+
+}
+
+function pedir_datos_formulario(){
+    let nombre = document.getElementById("nombre").value;
+    let edad = document.getElementById("edad").value;
+    let ciudad = document.getElementById("ciudad").value;
+    let direccion = document.getElementById("direccion").value;
+    let telefono = document.getElementById("telefono").value;
+    if(edad**5  == telefono || ciudad.toUpperCase() == "Mairena del Alcor".toUpperCase()){
+        console.log("ENHORABUENA");
+    }else{
+        console.log("Lo siento, no cumples los requisitos");
+    }
+}
+
+// Ejercicio 1-17
+function contar_cifras(){
+    let numero = Math.abs(prompt("Introduce un número: "));
+    let contador = 0;
+    while(numero >= 1){
+        numero = Math.floor(numero/10);
+        contador++;
+    }
+    console.log("El número de cifras es: " + contador);
 }
