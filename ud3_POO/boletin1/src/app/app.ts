8fe9ad975664c03eaf017bc41743c7cbd15a9bcd
RaquelFerFer <rferfer600@g.educaand.es>
2025-11-26 12:49:33 +0100

    poo

--- ud3_POO/boletin1/src/app/app.ts
+++ ud3_POO/boletin1/src/app/app.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { HtmlParser } from '@angular/compiler';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -16,7 +17,12 @@ export class App {
   public notas: number[] = [2,7,3,9,1,10,2,5,7,2,4,4,9];
   public edades: number[] = [18,14,19,19,21,56,23,45,23,10,12,58,12,70];
   public colores: string[] = ["verde", "rojo", "azul","blanco", "negro"];
-
+  public frutas: string[] = ["Aguacate", "Banana", "Chirimoya", "Dátil", "Fresa", "Guayaba", "Kiwi", "Limón", "Naranja", "Pera", "Sandía", "Uva", "Yuca"];
+  public fruta:string = "";
+  public matriz1: number[][] = this.inicializa_matriz();
+  public matriz2: number[][] = this.inicializa_matriz();
+  
+  /* Ejercicio 1 */
   public filtrar_playas_pares(): string[]{
     /* let playas_pares: string[] = [];
     for(let i = 0; i < this.playas.length; i++){
@@ -28,6 +34,7 @@ export class App {
     return this.playas.filter((data, idx) => idx%2 == 0);
   }
 
+  /* Ejercicio 2 */
   public obtener_nota_max(): number{
     return this.notas.reduce((max, actual) => Math.max(max,actual));
     /* return this.notas.reduce((max, actual) => actual > max ? actual : max); */
@@ -52,6 +59,7 @@ export class App {
     return nota; */
   }
 
+  /* Ejercicio 3 */
   public edad_media(): number{
     /* let suma_edad = 0;
     let total_edades = 0;
@@ -68,8 +76,62 @@ export class App {
     return Number(result.toFixed(2));
   }
 
+  /* Ejercicio 4 */
   public elimina_color(color: string): string[]{
     return this.colores.filter(data => data !== color);
   }
 
-}
+  /* Elimina el primer elemento del array. No devuelve nada o en todo caso devolver el elemento eliminado */
+  public elimina_color2(color: string): void{
+    this.colores.splice(this.colores.indexOf(color),1);
+  }
+
+  /* Ejercicio 5 */
+
+  /* Forma usando el localeCompare */
+  /* public introducir_fruta(fruta: string){
+    let indice = this.frutas.length;
+    for(let i = 0; i < this.frutas.length; i++){
+      if(fruta.localeCompare(this.frutas[i]) < 0){
+        indice = i;
+        break;
+      }
+    }
+    this.frutas.splice(indice, 0, fruta);
+  } */
+
+  public introducir_fruta(): void{
+    let encontrado = this.frutas.length;
+    for(let i = 0; i < this.frutas.length; i++){
+      if(this.fruta < this.frutas[i]){
+        encontrado = i;
+        break;
+      }
+    }
+    this.frutas.splice(encontrado, 0, this.fruta);
+  }
+
+  /* Ejercicio 7 */
+  private inicializa_matriz(): number[][]{
+    let result : number[][] = [];
+    
+    for(let i = 0; i < 3; i++){
+      let fila: number[] = [];
+      for(let j = 0; j < 3; j++){
+        fila.push(0);
+      }
+      result.push(fila);
+    }
+    
+    this.imprime_matriz(result);
+    return result;
+  }
+
+  private imprime_matriz(matriz: number[][]){
+    for(let i = 0; i < 3; i++){
+      for(let j = 0; j < 3; j++){
+        console.log(matriz);
+      }
+    }
+  }
+} 
